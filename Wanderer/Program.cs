33680893bb741f65c5f90b09bb5e087f420fbbd2
ArunaThilakrathne <75773622+ArunaThilakrathne@users.noreply.github.com>
2023-01-14 11:34:58 +0530

    Portofolio with hero section

--- Wanderer/Program.cs
+++ Wanderer/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer
 (builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddApplicationInsightsTelemetry();
 //migration with nuget console
 //add-migration nameofmigration add nuget package entity framework core tools 
 //after that update-databse
